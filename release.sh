63f81975fb9f40092b821d3229c3de7e6c712afe
Martin Uhrin <martin.uhrin@gmail.com>
2021-05-21 12:33:05 +0200

    Updated release script

--- release.sh
+++ release.sh
@@ -15,7 +15,9 @@ while true; do
 done
 
 
-sed -i "1 s/__version__* =.*/__version__ = \"${version}\"/" $VERSION_FILE
+sed -i "0,/__version__* =.*/s//__version__ = \"${version}\"/" $VERSION_FILE
+
+exit
 
 current_branch=`git rev-parse --abbrev-ref HEAD`
 
